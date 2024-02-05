0961833f9ca31ca06b117d46726070649ce9023a
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-05 12:23:33 +0530

    add current product state for product details

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "./rootReducer.js";
+import { asyncCurrentProductFetch } from "./current-product/currentProductAction.js";
 
 
 export const store = createStore(rootReducer, composeWithDevTools(
     applyMiddleware(thunk)
 ))
+
+
+
 
 
 
