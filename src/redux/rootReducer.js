f3decca2fa67aedda645c5196d43ff82b38937fb
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-05 12:24:10 +0530

    add current product reducer in root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./product/productReducer";
+import { productReducer } from "./product/productReducer.js";
+import { currentProductReducer } from "./current-product/currentProductReducer.js";
 
 
 export const rootReducer = combineReducers({
     product: productReducer,
+    currentProduct: currentProductReducer
 })
