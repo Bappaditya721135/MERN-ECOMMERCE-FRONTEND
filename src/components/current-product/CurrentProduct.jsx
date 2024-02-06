c361c8564f891f7c9db4243a95f324a129f73aba
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-06 13:33:15 +0530

    create current Product jsx

--- src/components/current-product/CurrentProduct.jsx
+++ src/components/current-product/CurrentProduct.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { asyncCurrentProductFetch } from '../../redux/current-product/currentProductAction';
+import ReactStars from "react-rating-stars-component"
 
 const CurrentProduct = () => {
+    // QUANTITY 
+    const [quantity, setQuantity] = useState(1);
+
     const {loading, product: currentProduct, err}= useSelector(state => state.currentProduct);
     const dispatch = useDispatch();
     const {id} = useParams();
+
+    const onChangeHandler = (e) => {
+        setQuantity(e.target.value);
+    }
 
 
 
@@ -24,6 +32,18 @@ const CurrentProduct = () => {
     if(currentProduct) {
         // DESTRUCTURE ALL THE PROPERTIES 
         const {name, images, price, discription, numOfReviews, ratings, stocks} = currentProduct.product;
+        console.log(stocks)
+
+        // OPTIONS FOR REACT RATING STARS 
+        const options = {
+            value: ratings,
+            edit: false,
+            isHalf: true,
+            color: "rgba(20, 20, 20, 0.5)",
+            activeColor: "tomato",
+            size: window.innerWidth < 600 ? 20 : 25,
+        }
+
         return (
             <div className="current__product--container">
                 <Link to="/" className="go-back__link">
@@ -35,14 +55,26 @@ const CurrentProduct = () => {
                     </div>
                     <div className="product__details">
                         <h4 className="product__name">{name}</h4>
-                        <div className="product__ratings">{ratings} num of reviews{numOfReviews}</div>
+                        <div className="product__ratings">
+                            <ReactStars {...options} />
+                            <span>({numOfReviews} reviews)</span>
+                        </div>
                         <p className="product__price">₹{price}</p>
-                        <p className="product__discription">{discription}</p>
+                        <div className="product__addToCart-section">
+                            <div className="quantity__section">
+                                <button>de</button>
+                                <input type="number" name="quantity" id="quantity" onChange={onChangeHandler} value={quantity} />
+                                <button>in</button>
+                            </div>
+                            <button className="btn__addToCart">add to cart</button>
+                        </div>
+                        <p className="product__status">status: {stocks > 0 ? <span className="stocks-in">InStocks</span> : <span className="stocks-out">OutOfStocks</span>}</p>
+                        <div className="product__discription-section">
+                            <span className="discription__title">Discription</span>
+                            <p className="product__discription">{discription}</p>
+                        </div>
+                        <button className="btn__add-review">add review</button>
                     </div>
-                </div>
-                <div className="product__buy-section">
-                    <button className="btn__addToCart">add to cart</button>
-                    <button className="btn__buyNow">buy now</button>
                 </div>
             </div>
           )
