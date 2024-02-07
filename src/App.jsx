609fc3e8e4b8b355c10d20dd38ba92df74081693
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-07 08:09:42 +0530

    add route for all product

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import Home from './components/home/Home';
 import "../src/styles/styles.scss";
 import CurrentProduct from './components/current-product/CurrentProduct';
 import Header from './components/layouts/header/Header';
+import AllProducts from './components/all-products/AllProducts';
 
 // CREATING A ROUTER 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Header />} >
     <Route index element={<Home />} />
     <Route path="product/:id" element={<CurrentProduct />} />
+    <Route path="products" element={<AllProducts />} />
+    <Route path="products/product/:id" element={<CurrentProduct />} />
   </Route>
 ))
 
