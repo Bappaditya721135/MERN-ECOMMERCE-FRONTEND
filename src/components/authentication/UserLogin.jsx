01730e1a85ef79d6bc40572787cb4e9b844ee4fe
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-15 12:00:47 +0530

    add redirect functionality in login

--- src/components/authentication/UserLogin.jsx
+++ src/components/authentication/UserLogin.jsx
@@ -1,15 +1,30 @@
 import axios from 'axios'
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { asyncLogin } from '../../redux/authentication/loginAction'
+import {  useNavigate, useSearchParams, Link } from 'react-router-dom'
 
 const UserLogin = () => {
+  const {isAuthenticated} = useSelector(state => state.user);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate()
+  const message = searchParams.get("message")
+  const path = searchParams.get("pathname") || "/";
+
     const [user, setUser] = useState({
         email: "",
         password: "",
     })
 
     const dispatch = useDispatch()
+
+
+    // CHECK IF ISAUTHENTICATED IS TRUE 
+    useEffect(() => {
+      if(isAuthenticated) {
+        navigate(path);
+      }
+    }, [isAuthenticated])
 
     const handleInputChange = (e) => {
         setUser(prev => {
@@ -29,6 +44,8 @@ const UserLogin = () => {
       <form onSubmit={handleSubmit}>
         <input type="email" name="email" id="email" onChange={handleInputChange} value={user.email} />
         <input type="password" name="password" id="password" onChange={handleInputChange} value={user.password} />
+        {message && <h2>{message}</h2>}
+        <p>new here?<Link to="/register">create new account</Link></p>
         <button>login</button>
       </form>
     </div>
