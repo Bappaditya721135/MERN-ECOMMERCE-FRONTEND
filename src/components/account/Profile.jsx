cd3c2e9bc2c7363dcff602c86069a5a916b5f4c3
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-17 18:50:49 +0530

    add links for edit-profile orders change-password

--- src/components/account/Profile.jsx
+++ src/components/account/Profile.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { asyncLogout } from '../../redux/authentication/logoutAction'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom'
 
 
 
 
 const Profile = () => {
-    const {isAuthenticated} = useSelector(state => state.user);
+    const {isAuthenticated, user, loading, error} = useSelector(state => state.user);
+    console.log(isAuthenticated)
+    console.log(user)
+    console.log(error)
+    console.log(loading)
     const dispatch = useDispatch()
 
     
@@ -16,22 +20,38 @@ const Profile = () => {
         dispatch(asyncLogout())
     }
 
-    const style = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-        height: "100vh"
-    }
+
   if(!isAuthenticated) {
     console.log("navigating to login page")
     return <Navigate to="/login?message=you are not logged in&pathname=/profile" />
   }
 
   return (
-    <div style={style}>
-      <h2>this is logout</h2>
+    <div className="profile--container">
+      <h3 className="profile__title">My Profile</h3>
+      <div className="profile">
+      <div className="profile-img--section">
+        <div className="img--container"></div>
+      </div>
+      <div className="profile__details">
+        <div className="name__section">
+          <p>Full Name</p>
+          <p className="profile__name">{user.name}</p>
+        </div>
+        <div className="email__section">
+          <p>Email</p>
+          <p className="profile__email">{user.email}</p>
+        </div>
+        <div className="link__section">
+          <Link to="/orders">my orders</Link>
+          <Link to="/change-password">change password</Link>
+        </div>
+      </div>
+      <div className="logout-edit--section">
+      <Link to="/edit-profile">edit profile</Link>
       <button onClick={handleLogoutClick}>logout</button>
+      </div>
+    </div>
     </div>
   )
 }
