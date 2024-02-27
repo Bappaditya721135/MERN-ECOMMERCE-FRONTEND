05d5d2fc5d08ceaee8611fe4655a81b14e2ad091
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-27 11:55:05 +0530

    add addToCart reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import { productReducer } from "./product/productReducer.js";
 import { currentProductReducer } from "./current-product/currentProductReducer.js";
 import { authenticationReducer } from "./authentication/authenticationReducer.js";
+import { addToCartReducer } from "./add to cart/addToCartReducer.js";
 
 
 export const rootReducer = combineReducers({
     product: productReducer,
     currentProduct: currentProductReducer,
     user: authenticationReducer,
+    addToCart: addToCartReducer,
 })
