8c97a01be739041ff922797146d38708392c3a2a
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-02-27 12:02:57 +0530

    create profile update action component

---
+++ src/redux/profile/profileUpdateAction.js
@@ -0,0 +1,14 @@
+// import axios from "axios";
+
+// export const PROFILE_UPDATE_FETCH = "PROFILE_UPDATE_FETCH";
+// export const PROFILE_UPDATE_SUCCESS = "PROFILE_UPDATE_SUCCESS";
+// export const PROFILE_UPDATE_ERROR = "PROFILE_UPDATE_ERROR";
+
+
+
+// export const profileUpdate = async (updatedUserData) => {
+//     return (dispatch) => {
+//         dispatch({type: PROFILE_UPDATE_FETCH})
+//         axios.
+//     }
+// }
