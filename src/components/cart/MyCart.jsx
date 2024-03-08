4fb73f45559fcbd07b07633a731c9098251382dc
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-03-08 19:49:49 +0530

    create cart and cart item card component

---
+++ src/components/cart/MyCart.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom';
+import { getMyCart } from '../../redux/my cart/myCartAction';
+import CartItemCard from './cart-item-card/CartItemCard';
+
+function MyCart() {
+    const {isAuthenticated} = useSelector(state => state.user);
+    const {loading, cart, error} = useSelector(state => state.myCart);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(getMyCart())
+
+    }, [])
+
+
+    
+
+    if(!isAuthenticated) {
+        return <Navigate to="/login?message=you must login first &pathname=/my-cart" />
+    }
+  return (
+    <div className="cart__section">
+      <h2>my cart </h2>
+
+        <div className="cart__container">
+          {cart && 
+              cart.length > 0 ? 
+              cart.map((cartItem, i) => {
+                console.log("map")
+                console.log(cartItem.product)
+                  return <CartItemCard key={i} product={cartItem.product} quantity={cartItem.quantity} />
+              }) : <p className="default-text--U">you don't have any items in your cart</p>}
+
+            <div className="cart-order">
+              {/* <p>total price: {cart ? getTotalPrice(cart) : 0}</p> */}
+              <button>place order</button>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default MyCart
