7e518079ce5621e1a148da1ce4bc61d57d8c1857
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-03-08 19:47:20 +0530

    fix files import

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 // COMPONENTS  
 import Home from './components/home/Home';
-
-// SCSS 
-import "../src/styles/styles.scss";
 import CurrentProduct from './components/current-product/CurrentProduct';
 import Header from './components/layouts/header/Header';
 import AllProducts from './components/all-products/AllProducts';
@@ -16,6 +13,10 @@ import Profile from "./components/account/Profile"
 import MyOrder from './components/orders/MyOrder';
 import ChangePassword from './components/password/ChangePassword';
 import EditProfile from './components/account/EditProfile';
+import MyCart from './components/cart/MyCart';
+
+// SCSS 
+import "../src/styles/styles.scss";
 
 // CREATING A ROUTER 
 const router = createBrowserRouter(createRoutesFromElements(
@@ -31,6 +32,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/orders" element={<MyOrder />} />
     <Route path="/change-password" element={<ChangePassword />} />
     <Route path="/edit-profile" element={<EditProfile />} />
+    <Route path="/my-cart" element={<MyCart />} />
   </Route>
 ))
 
