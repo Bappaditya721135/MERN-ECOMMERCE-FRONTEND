17e4cae9b9b07dd0481a200a567935cd3e3aca19
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-04-02 18:53:33 +0530

    add proxy url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 
 const localUrl = "http://localhost:5000";
-const url = "https://mern-ecommerce-backend-khaki.vercel.app/"
+const url = "https://mern-ecommerce-backend-khaki.vercel.app"
 
 // https://vitejs.dev/config/
 export default defineConfig({
